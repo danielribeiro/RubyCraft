48e44edb4a266cb79b7440b094b145b519eb5011
Daniel Ribeiro <danrbr@gmail.com>
2011-07-11 02:23:58 -0300

    still a test. Bug just created my 0-0 eldorado

---
+++ lib/mnedit.rb
@@ -0,0 +1,302 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'active_support'
+require 'nbtfile'
+require 'stringio'
+require 'zlib'
+require 'block'
+
+class Integer
+  def bytes
+    [self >> 24, (self >> 16) & 0xFF, (self >> 8) & 0xFF, self & 0xFF]
+  end
+end
+
+module ArrayExtension
+  def add_all(enum)
+    for i in enum
+      self << i
+    end
+    self
+  end
+
+  # To be used when the array is used as a byte array
+  def toIo
+    io = StringIO.new
+    io.write self.pack('C*')
+    io.rewind
+    io
+  end
+end
+
+class Array
+  include ArrayExtension
+end
+
+class String
+  # To be used when string is used as a byte array
+  def toIo
+    byteArray.toIo
+  end
+
+  def byteArray
+    bytes.to_a
+  end
+end
+
+class NBTFile::Private::Tokenizer
+  def initialize(io)
+    @gz = io
+    @state = NBTFile::Private::TopTokenizerState.new()
+  end
+end
+
+class NBTFile::Emitter
+  def initialize(stream)
+    @gz = stream
+    @state = NBTFile::Private::TopEmitterState.new()
+  end
+end
+
+class PlaneCounter
+  def initialize
+    @x = 0
+    @z = 0
+  end
+
+  def inc
+    @x += 1
+    if @x == 32
+      @x = 0
+      @z += 1
+    end
+    pos
+  end
+
+  def pos
+    [@x, @z]
+  end
+
+end
+
+class ChunkCounter
+  def initialize
+    @y = 0
+    @z = 0
+    @x = 0
+  end
+
+  def inc
+    @y += 1
+    if @y == 128
+      @y = 0
+      @z += 1
+    end
+    if @z == 16
+      @z = 0
+      @x += 1
+    end
+    pos
+  end
+
+  def pos
+    [@y, @z, @x]
+  end
+
+end
+
+class Region
+  attr_accessor :bytes
+
+  def initialize(file)
+    @bytes = IO.read(file).byteArray
+    @file = file
+  end
+
+  def write
+    File.open(@file, "wb") do |f|
+      f << bytes.pack("C*")
+    end
+    self
+  end
+
+  def printspecs
+    counter = PlaneCounter.new
+    bytes[0..4095].each_slice(4) do |ar|
+      p ar
+      offset = bytesToInt [0] + ar[0..-2]
+      count = ar.last
+      x, y = counter.pos
+      puts "[#{x}, #{y}] : #{offset}, count = #{count}"
+      counter.inc
+    end
+  end
+
+  def dirtyToGold
+    chunkMetaDataSize = 5
+    dummytimestamp = 0
+    chunks = []
+    bytes[0..4095].each_slice(4) do |ar|
+      offset = bytesToInt [0] + ar[0..-2]
+      count = ar.last
+      if count > 0
+        chunks << chunkToGold(offset)
+      else
+        chunks << nil
+      end
+    end
+    newBytes = []
+    lastVacantPosition = 2
+    for chunk in chunks
+      if chunk
+        offset = lastVacantPosition
+        sizeCount = ((chunk.size + chunkMetaDataSize).to_f / 4096).ceil
+        lastVacantPosition += sizeCount
+        newBytes.add_all offset.bytes[1..3]
+        newBytes << sizeCount
+      else
+        pad newBytes, 4
+      end
+    end
+    defaultCompressionType = 2
+    pad newBytes, 4096, dummytimestamp
+    for chunk in chunks
+      next if chunk.nil?
+      newBytes.add_all((chunk.size + 1).bytes)
+      newBytes << defaultCompressionType
+      newBytes.add_all chunk
+      size = (chunk.size + chunkMetaDataSize)
+      remaining = 4096 - (size % 4096)
+      pad newBytes, remaining % 4096
+    end
+    File.open(@file, "wb") do |f|
+      f << newBytes.pack("C*")
+    end
+  end
+
+  def pad(array, count, value = 0)
+    count.times do
+      array << value
+    end
+    array
+  end
+
+  def chunkToGold(offset)
+    o = offset * 4096
+    bytecount = bytesToInt bytes[o..(o + 4)]
+    o += 5
+    nbtBytes = bytes[o..(o + bytecount - 2)]
+    offset = o
+    name, body = readnbt nbtBytes
+    blocks = body['Level']['Blocks']
+    newarray = blocks.value.bytes.map do |b|
+      if 1 <= b and b <= 3
+        41
+      else
+        b
+      end
+    end
+    body['Level']['Blocks'] = NBTFile::Types::ByteArray.new newarray.pack("C*")
+    output = StringIO.new
+    NBTFile.write(output, name, body)
+    out = compress(output.string).byteArray
+    return out
+  end
+
+  def readChunk(x, z)
+    o = 4 * (x + z * 32)
+    offset = bytesToInt [0] + bytes[o..(o + 2)]
+    puts "Its sector count is  #{bytes[o + 3]}"
+    puts "the offset is: #{offset}"
+    o = offset * 4096
+    bytecount = bytesToInt bytes[o..(o + 4)]
+    puts "It has size: #{bytecount}"
+    o += 5
+    nbtBytes = bytes[o..(o + bytecount - 2)]
+    offset = o
+    printNbt(nbtBytes)
+  end
+
+  def printNbt(nbtBytes)
+    name, body = readnbt nbtBytes
+    blocks = body['Level']['Blocks']
+    counter = ChunkCounter.new
+    blocks.value.bytes.map do |b|
+      name = Block.get(b).name
+      puts "#{counter.pos.inspect}: #{name}"
+      counter.inc
+    end
+    puts "-> Entities"
+    entities = body['Level']['Entities']
+    puts entities.to_json
+
+    puts "-> Height map"
+    heightmap = body['Level']['HeightMap']
+    x = 0
+    z = 0
+    heightmap.value.bytes.map do |h|
+      puts "At [#{x}, #{z}]: #{h}"
+      x += 1
+      if x == 16
+        z += 1
+        x = 0
+      end
+    end
+
+    puts "-> LastUpdate: #{body['Level']['LastUpdate'].value }"
+  end
+
+  def change(x, z)
+    o = 4 * (x + z * 32)
+    offset = bytesToInt [0] + bytes[o..(o + 2)]
+    o = offset * 4096
+    bytecount = bytesToInt bytes[o..(o + 4)]
+    o += 5
+    nbtBytes = bytes[o..(o + bytecount - 2)]
+    offset = o
+    name, body = readnbt nbtBytes
+    blocks = body['Level']['Blocks']
+    newarray = blocks.value.bytes.map do |b|
+      8
+    end
+    body['Level']['Blocks'] = NBTFile::Types::ByteArray.new newarray.pack("C*")
+    output = StringIO.new
+    NBTFile.write(output, name, body)
+    @bytes[offset..(offset + bytecount - 2)] = compress(output.string).byteArray
+    self
+  end
+
+  protected
+  def bytesToInt(array)
+    array.pack('C*').unpack("N").first
+  end
+
+  def readnbt(bytes)
+    NBTFile.read Zlib::Inflate.inflate(bytes.toIo.read).toIo
+  end
+
+  def compress(str)
+    Zlib::Deflate.deflate(str)
+  end
+
+end
+
+puts "Starting"
+reg = '/home/daniel/.minecraft/saves/newone/region/r.0.0.mcr'
+#reg = '/home/daniel/.minecraft/saves/Hacking/region/r.0.0.mcr'
+#Region.new(reg).change(0, 0).change(0, 1).change(1, 1).change(1, 0)
+#Region.new(reg).readChunk 0, 0
+#Region.new(reg).printspecs
+Region.new(reg).dirtyToGold
+
+puts "moved!"
+
+
+#for x in 0..15
+#  for z in 0..15
+#    for y in 0..127
+#      val = y + z * 128 + x * 128 * 16
+#      puts "[#{y}, #{z}, #{x}] -> #{val}"
+#    end
+#  end
+#end
