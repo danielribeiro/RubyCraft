eaaf61148ed671b4342907a26f4240ea0a0d76c9
Daniel Ribeiro <danrbr@gmail.com>
2011-07-21 23:55:38 -0300

    first iteration mechanism is done

--- lib/chunk.rb
+++ lib/chunk.rb
@@ -4,7 +4,20 @@ require 'nbt_helper'
 require 'byte_converter'
 
 class Chunk
-  def initialize(nbtBody)
-    @nbtBody = nbtBody
+  def initialize(nbtData)
+    name, @nbtBody = nbtData
+  end
+
+  # Converts all blocks on data do another type
+  def block_map!(&block)
+    newData = @nbtBody["Blocks"].value.bytes.map do |byte|
+      ret = block.call Block.get(byte).name
+      Block[ret].id
+    end
+    @nbtBody["Data"] = NBTFile::Types::ByteArray.new ByteConverter.toByteString newData
+  end
+
+  def export
+    ["", @nbtBody]
   end
 end
--- spec/chunk_spec.rb
+++ spec/chunk_spec.rb
@@ -9,12 +9,27 @@ describe Chunk do
     NBTFile::Types::ByteArray.new toByteString array
   end
 
-  it "should desc" do
+  it "can use to change all block to another type" do
     data = NBTFile::Types::Compound.new
     data['HeightMap'] = byteArray [100] * 256
     data["Blocks"] = byteArray [Block[:stone].id] * 32768
-    chunk = Chunk.new(data)
+    data["Data"] = byteArray [0] * 16384
+    chunk = Chunk.new(["", data])
+    chunk.block_map! do
+      :gold
+    end
+    name, newData = chunk.export
+    newData["Data"].value.should == toByteString([Block[:gold].id] * 32768)
 
   end
+
+#  it "can iterate over planes"
+#  it "can iterate over lines"
+#  it "can iterate over cubes"
+#  it "can iterate over blocks with data"
+#  it "corrects height map"
+#  it "iterate using either blockname or block full data"
+#  it "maybe uses block as immutable or as mutable."
 end
+
 
